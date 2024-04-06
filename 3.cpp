cb48ae63b7e9256593f5751a4e3c2d25bdc816ca
Pradeep Kumar <pkkashyap2004@gmail.com>
2024-04-07 00:05:10 +0530

    This main branch add text

---
+++ 3.cpp
@@ -0,0 +1,9 @@
+/*Write a program to indicate the use of object in class.*/
+#include<iostream>
+using namespace std;
+
+int main(){
+ 
+    cout<<"This is main branch so its working important tasks; ";
+   
+}
